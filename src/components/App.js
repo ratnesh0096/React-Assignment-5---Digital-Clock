e4345a6d63c02264eed3c3db7e6bff759c908b4a
ratnesh0096 <ratneshmether09.rm@gmail.com>
2021-04-16 23:28:11 +0530

    Final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
+    constructor(props) {
+        super(props)
+        // this.state={date:new Date()}
+        this.state = { hour: null }
+        this.state = { minute: null }
+        this.state = { second: null }
 
-        return(
-            <>
-               
-            </>
+    }
+    updateDate() {
+        // this.setState({date: new Date()})
+        this.setState({ hour: new Date().getHours() })
+        this.setState({ minute: new Date().getMinutes() })
+        this.setState({ second: new Date().getSeconds() })
+
+    }
+    componentDidMount() {
+        this.time = setInterval(() => { this.updateDate() }, 1000);
+    }
+    componentWillMount() {
+        clearInterval(this.time);
+    }
+    render() {
+        let hour = "";
+        let minute = this.state.minute;
+        let second = this.state.second;
+        this.state.hour > 12 ? hour = this.state.hour - 12 : hour = this.state.hour
+        this.state.hour < 10 ? hour = "0" + hour : hour;
+        this.state.minute < 10 ? minute = "0" + minute : minute;
+        this.state.second < 10 ? second = "0" + second : second;
+        return (
+            <div className="Clock">
+                <h3 id="time">{hour}:{minute}:{second} {this.state.hour<11 ? `AM` : `PM`}
+                </h3>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
+// function generateCodeFromObject(obj){
+//     return(
+//         <h1>Hello</h1>
+//     )
+//    }
    
-module.exports=generateCodeFromObject;
+// module.exports=generateCodeFromObject;
 
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
